0f5ddde8e8dcf4d4da603db7da60e83705518e70
Dimader0 <lazdima2008@gmail.com>
2024-01-07 16:31:07 +0200

    3

--- imageeditor/main.py
+++ imageeditor/main.py
@@ -4,6 +4,7 @@ from Pil import Image, ImageFilter
 app = QApplication([])
 main_win = QWidget()
 main_win.setWindowTitle("Графічний редактор")
+main_win.resize(700,500)
 lb_picture = QLabel("Зоображення")
 file_list = QListWidget()
 btn_open_folder = QPushButton("Папка")
@@ -32,8 +33,8 @@ col_1.addWidget(file_list)
 col_2.addWidget(lb_picture)
 col_2.addLayout(button_layout)
 
-main_layout.addLayout(col_1)
-main_layout.addLayout(col_2)
+main_layout.addLayout(col_1, 20)
+main_layout.addLayout(col_2, 80)
 main_win.setLayout(main_layout)
 
 
