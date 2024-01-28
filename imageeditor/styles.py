e9602c3295dbe7d93fb36af8089255dc5f10e742
Gotliha <145850039+Gotliha@users.noreply.github.com>
2024-01-28 17:28:43 +0200

    11

--- imageeditor/styles.py
+++ imageeditor/styles.py
@@ -3,4 +3,12 @@ style = """
     QWidget{
         background: rgb(60, 120, 150);
     }
+    QListWidget{
+        border: 3px solid black;
+        color: white;
+    }
+    QPushButton{
+        color: rgb(123, 209, 82);
+        border: 1px solid black;
+    }
 """
