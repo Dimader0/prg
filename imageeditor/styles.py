4c48b811ce703eec62e162d4bf48af74f8092b54
Gotliha <145850039+Gotliha@users.noreply.github.com>
2024-02-04 16:47:25 +0200

    8

--- imageeditor/styles.py
+++ imageeditor/styles.py
@@ -2,13 +2,24 @@ style = """
 
     QWidget{
         background: rgb(60, 120, 150);
+        fond: normal small-caps normal 20px/1.4 sans-serif;
     }
     QListWidget{
         border: 3px solid black;
         color: white;
     }
+    QListWidget::item:selected{
+        color: #7a369e;
+    }
     QPushButton{
         color: rgb(123, 209, 82);
         border: 1px solid black;
+    }
+    QPushButton:hover{
+        color: #111111;
+        border: 2px solid black;
+    }
+    QPushButton:pressed{
+        color: #4ab553;
     }
 """
